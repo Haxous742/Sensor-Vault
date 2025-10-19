0d99afb7c9c394854826bf518d91a12a802c4be4
aryansharma0305 <aryansharma0305@gmail.com>
2025-10-19 23:10:03 +0530

    fdxvgsdf

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ let io; // to hold the socket instance
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "*",
       methods: ["GET", "POST"],
     },
   });
