5c67dc7d8d6789b81614d1a26b5ea20bf1822cb9
aryansharma0305 <aryansharma0305@gmail.com>
2025-10-20 01:15:58 +0530

    .

--- backend/models/Teams.js
+++ backend/models/Teams.js
@@ -19,12 +19,34 @@ const teamSchema = new Schema(
         email: { type: String, required: true, lowercase: true},
       },
     ],
-    
 
+    isDone: { type: Boolean, default: false },
 
+    result: { type: Boolean, default: false },
 
+    timeTaken: { type: Number, default: 0 }, // in seconds
 
+    task1CorrectAnswer: { type: String },
+    task1CurrentAnswer:{ type: String  },
+    task1timeTaken: { type: Number, default: 0 },
+    task1Done: { type: Boolean, default: false },
 
+    task2CorrectAnswer: { type: String },
+    task2CurrentAnswer:{ type: String  },
+    task2timeTaken: { type: Number, default: 0 },
+    task2Done: { type: Boolean, default: false },
+
+    task3CorrectAnswer: { type: String },
+    task3CurrentAnswer:{ type: String  },
+    task3timeTaken: { type: Number, default: 0 },
+    task3Done: { type: Boolean, default: false },
+
+    task4CorrectAnswer: { type: String },
+    task4CurrentAnswer:{ type: String  },
+    task4timeTaken: { type: Number, default: 0 },
+    task4Done: { type: Boolean, default: false },
+
+    startedAt: { type: Date },
 
   },
   
